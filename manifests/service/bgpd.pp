98546dc464c390f582671e981744ddafa8e68fa8
b4ldr <github@johnbond.org>
2016-04-05 23:10:11 -0300

    remove duplicat networks

--- manifests/service/bgpd.pp
+++ manifests/service/bgpd.pp
@@ -1,46 +1,5 @@
 # Class: quagga::service::bgpd
 #
-# BGPd for the Quagga routing server.
-#
-# Parameters:
-#  $content:
-#    Content (typically using a template) for the bgpd.conf file.
-#  $router_id:
-#    The router id to use
-#  $networks4:
-#    an array of ipv4 networks to advertise
-#  $networks6:
-#    an array of ipv6 networks to advertise
-#  $hultihop:
-#    the hop count for a multihop peering
-#  $enable_advertisements:
-#    boolean to indecate if we should advertise networks
-#  $peers:
-#    A hash of peers to configure of the form
-#    AS_Number :
-#        addr4:
-#             - 1st ipv4 neighbour address
-#             - 2nd ipv4 neighbour address
-#        addr6:
-#             - 1st ipv6 neighbour address
-#             - 2nd ipv6 neighbour address
-#        localpref: the local pref to assign prefix recived
-#
-# Sample Usage :
-#  class { 'quagga::service::bgpd':
-#    myasn                 => 65535,
-#    router_id             => '192.0.2.1',
-#    networks4             => ['192.0.2.0/24'],
-#    networks6             => ['2001:DB8::/32'],
-#    enable_advertisements => true,
-#    peers                          => { 65536 => {
-#                                   addr4 => ['192.0.2.2'],
-#                                   addr6 => ['2001:DB8::1'],
-#                                   multihop => 5,
-#                                   localpref => 120 } }
-#    content               => template('mymodule/quagga/bgpd.conf.erb'),
-#  }
-#
 class quagga::service::bgpd (
   $my_asn                   = undef,
   $router_id                = undef,
