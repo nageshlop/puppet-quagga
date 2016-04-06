8a6002e25daad005027013c3ac358c5e319d4e4f
b4ldr <github@johnbond.org>
2016-04-05 22:52:04 -0300

    add peer

---
+++ manifests/service/bgpd/peer.pp
@@ -0,0 +1,34 @@
+# quagga::service::bgpd::peer
+#
+define quagga::service::bgpd::peer (
+  $addr4          = [],
+  $addr6          = [],
+  $desc           = undef,
+  $inbound_routes = 'none',
+  $community      = undef,
+  $localpref      = undef,
+  $multihop       = undef,
+  $password       = undef,
+  $prepend        = undef,
+) {
+  validate_array($addr4)
+  validate_array($addr6)
+  validate_string($desc)
+  validate_re($inbound_routes, '^(all|none|default)$')
+  if $community { validate_array($community) }
+  if $localpref { validate_integer($localpref) }
+  if $multihop { validate_integer($multihop) }
+  if $password { validate_string($password) }
+  if $prepend { validate_integer($prepend) }
+  
+  concat::fragment{"bgpd_peer_${name}":
+    target  => $::quagga::service::bgpd::conf_file,
+    content => template('quagga/bgpd.conf.peer.erb'),
+    order   => 10,
+  }
+  concat::fragment{"bgpd_v6peer_${name}":
+    target  => $::quagga::service::bgpd::conf_file,
+    content => template('quagga/bgpd.conf.v6peer.erb'),
+    order   => 40,
+  }
+}
