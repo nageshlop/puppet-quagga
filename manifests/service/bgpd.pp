d0ab358ac449e356f70a09199db95e9da0b328f5
b4ldr <github@johnbond.org>
2016-04-05 22:50:31 -0300

    refactor to use concat and validate peers

--- manifests/service/bgpd.pp
+++ manifests/service/bgpd.pp
@@ -54,21 +54,42 @@ class quagga::service::bgpd (
   $enable_advertisements_v6 = true,
   $manage_nagios            = false,
   $peers                    = undef,
-  $content                  = undef,
+  $conf_file                = '/etc/quagga/bgpd.conf',
 ) {
 
-  include quagga::params
+  class { '::quagga': }
 
   validate_bool($manage_nagios)
 
-  $content_real = $content ? {
-    undef   => $::quagga::params::bgpd_content,
-    default => $content,
+  concat{$conf_file:
+    require => Package[ $::quagga::package ],
+    notify  => Service['quagga'],
   }
-
-  class { 'quagga::service':
-    service  => 'bgpd',
-    content  => template($content_real)
+  concat::fragment{ 'quagga_bgpd_head':
+    target  => $conf_file,
+    content => template('quagga/bgpd.conf.head.erb'),
+    order   => 01,
   }
+  concat::fragment{ 'quagga_bgpd_v6head':
+    target  => $conf_file,
+    content => "!\naddress-family ipv6\n",
+    order   => 30,
+  }
+  concat::fragment{ 'quagga_bgpd_v6foot':
+    target  => $conf_file,
+    content => template('quagga/bgpd.conf.v6foot.erb'),
+    order   => 50,
+  }
+  concat::fragment{ 'quagga_bgpd_acl':
+    target  => $conf_file,
+    content => template('quagga/bgpd.conf.acl.erb'),
+    order   => 90,
+  }
+  concat::fragment{ 'quagga_bgpd_foot':
+    target  => $conf_file,
+    content => "!\nline vty\n",
+    order   => 99,
+  }
+  create_resources(quagga::service::bgpd::peer, $peers)
 }
 
