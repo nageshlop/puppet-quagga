9cb94f6adbd690acba7eed5489598a6ed715b21a
b4ldr <github@johnbond.org>
2016-05-04 15:51:35 +0100

    correct ipv6 test

--- spec/acceptance/quagga_multi_spec.rb
+++ spec/acceptance/quagga_multi_spec.rb
@@ -6,6 +6,11 @@ describe 'quagga class multi router' do
   router2 = find_host_with_role(:router2)
   router1_ip = fact_on(router1, "ipaddress")
   router2_ip = fact_on(router2, "ipaddress")
+  router1_ip6 = '2001:db8:1::1'
+  router2_ip6 = '2001:db8:1::2'
+  ipv6_network = '2001:db8:1::/64'
+  on(router1, "ip -6 addr add #{router1_ip6}/64 dev eth0")
+  on(router2, "ip -6 addr add #{router2_ip6}/64 dev eth0")
   context 'basic' do
     it 'should work with no errors' do 
       pp1 = <<-EOF
@@ -14,9 +19,11 @@ describe 'quagga class multi router' do
       my_asn => 64496,
       router_id => '#{router1_ip}',
       networks4 => [ '#{router1_ip.sub(/\d+$/,'0/24')}'],
+      networks6 => [ '#{ipv6_network}'],
       peers => {
         '64497' => {
           'addr4' => ['#{router2_ip}'],
+          'addr6' => ['#{router2_ip6}'],
           'desc'  => 'TEST Network'
           }
       }
@@ -28,9 +35,11 @@ describe 'quagga class multi router' do
       my_asn => 64497,
       router_id => '#{router2_ip}',
       networks4 => [ '#{router2_ip.sub(/\d+$/,'0/24')}'],
+      networks6 => [ '#{ipv6_network}'],
       peers => {
         '64496' => {
           'addr4' => ['#{router1_ip}'],
+          'addr6' => ['#{router1_ip6}'],
           'desc'  => 'TEST Network'
           }
       }
@@ -42,6 +51,9 @@ describe 'quagga class multi router' do
       expect(apply_manifest_on(router2, pp2,  :catch_failures => true).exit_code).to eq 0
       #allow peers to configure and establish
       sleep(5)
+    end
+    describe command('cat /etc/quagga/bgpd.conf 2>&1' ) do
+      its(:stdout) { should match(//) }
     end
     describe service('quagga') do
       it { is_expected.to be_running }
@@ -56,10 +68,19 @@ describe 'quagga class multi router' do
     describe command("ping -c 1 #{router2_ip}") do 
       its(:exit_status) { should eq 0 }
     end
+    describe command("ping6 -I eth0 -c 1 #{router2_ip6}") do 
+      its(:exit_status) { should eq 0 }
+    end
     describe command('vtysh -c \'show ip bgp sum\'') do
       its(:stdout) { should match(/#{router2_ip}\s+4\s+64497/) }
     end
     describe command("vtysh -c \'show ip bgp neighbors #{router2_ip}\'") do
+      its(:stdout) { should match(/BGP state = Established/) }
+    end
+    describe command('vtysh -c \'show ipv6 bgp sum\'') do
+      its(:stdout) { should match(/#{router2_ip6}\s+4\s+64497/i) }
+    end
+    describe command("vtysh -c \'show ip bgp neighbors #{router2_ip6}\'") do
       its(:stdout) { should match(/BGP state = Established/) }
     end
   end
