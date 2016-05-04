75a79edf2b11b2353f7dc2d2f766fb864d378e90
b4ldr <github@johnbond.org>
2016-05-04 16:09:12 +0100

    add ipv6 checks

--- spec/acceptance/quagga_multi_spec.rb
+++ spec/acceptance/quagga_multi_spec.rb
@@ -5,10 +5,13 @@ describe 'quagga class multi router' do
   router1 = find_host_with_role(:router1)
   router2 = find_host_with_role(:router2)
   router1_ip = fact_on(router1, "ipaddress")
-  router2_ip = fact_on(router2, "ipaddress")
   router1_ip6 = '2001:db8:1::1'
+  router1_asn = 64496
+  router2_ip = fact_on(router2, "ipaddress")
   router2_ip6 = '2001:db8:1::2'
+  router2_asn = 64497
   ipv6_network = '2001:db8:1::/64'
+  ipv4_network = router1_ip.sub(/\d+$/,'0/24')
   on(router1, "ip -6 addr add #{router1_ip6}/64 dev eth0")
   on(router2, "ip -6 addr add #{router2_ip6}/64 dev eth0")
   context 'basic' do
@@ -16,12 +19,12 @@ describe 'quagga class multi router' do
       pp1 = <<-EOF
     class { '::quagga': }
     class { '::quagga::bgpd': 
-      my_asn => 64496,
+      my_asn => #{router1_asn},
       router_id => '#{router1_ip}',
-      networks4 => [ '#{router1_ip.sub(/\d+$/,'0/24')}'],
+      networks4 => [ '#{ipv4_network}'],
       networks6 => [ '#{ipv6_network}'],
       peers => {
-        '64497' => {
+        '#{router2_asn}' => {
           'addr4' => ['#{router2_ip}'],
           'addr6' => ['#{router2_ip6}'],
           'desc'  => 'TEST Network'
@@ -32,12 +35,12 @@ describe 'quagga class multi router' do
       pp2 = <<-EOF
     class { '::quagga': }
     class { '::quagga::bgpd': 
-      my_asn => 64497,
+      my_asn => #{router2_asn},
       router_id => '#{router2_ip}',
-      networks4 => [ '#{router2_ip.sub(/\d+$/,'0/24')}'],
+      networks4 => [ '#{ipv4_network}'],
       networks6 => [ '#{ipv6_network}'],
       peers => {
-        '64496' => {
+        '#{router1_asn}' => {
           'addr4' => ['#{router1_ip}'],
           'addr6' => ['#{router1_ip6}'],
           'desc'  => 'TEST Network'
@@ -72,16 +75,22 @@ describe 'quagga class multi router' do
       its(:exit_status) { should eq 0 }
     end
     describe command('vtysh -c \'show ip bgp sum\'') do
-      its(:stdout) { should match(/#{router2_ip}\s+4\s+64497/) }
+      its(:stdout) { should match(/#{router2_ip}\s+4\s+#{router2_asn}/) }
     end
     describe command("vtysh -c \'show ip bgp neighbors #{router2_ip}\'") do
       its(:stdout) { should match(/BGP state = Established/) }
     end
+    describe command("vtysh -c \'show ip bgp neighbors #{router2_ip} advertised-routes\'") do
+      its(:stdout) { should match(/#{ipv4_network}\s+#{router1_ip}\s+0\s+32768\s+i/) }
+    end
     describe command('vtysh -c \'show ipv6 bgp sum\'') do
-      its(:stdout) { should match(/#{router2_ip6}\s+4\s+64497/i) }
+      its(:stdout) { should match(/#{router2_ip6}\s+4\s+#{router2_asn}/i) }
     end
     describe command("vtysh -c \'show ip bgp neighbors #{router2_ip6}\'") do
       its(:stdout) { should match(/BGP state = Established/) }
+    end
+    describe command("vtysh -c \'show ipv6 bgp neighbors #{router2_ip6} advertised-routes\'") do
+      its(:stdout) { should match(/#{ipv6_network}\s+#{router1_ip6}\s+0\s+32768\s+i/) }
     end
   end
 end
