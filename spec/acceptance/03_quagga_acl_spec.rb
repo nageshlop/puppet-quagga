45b348b7e0f48fe70dedb536207d3e0e3c043699
John Bond <github@johnbond.org>
2017-09-04 18:35:55 +0100

    add rejectedv4 and rejectedv6 tests

--- spec/acceptance/03_quagga_acl_spec.rb
+++ spec/acceptance/03_quagga_acl_spec.rb
@@ -11,10 +11,12 @@ describe 'quagga class ACLs ' do
   router2_ip = fact_on(router2, 'ipaddress')
   router2_ip6 = '2001:db8:1::2'
   router2_asn = '64497'
-  ipv6_network = '2001:db8:1::/64'
+  ipv6_network = '2001:db8:1::/48'
   ipv4_network = router1_ip.sub(%r{\d+$}, '0/24')
-  additional_v4_network =  '192.0.2.0/24'
-  additional_v6_network =  '2001:db8:2::/48'
+  additional_v4_network1 = '192.0.2.0/24'
+  additional_v6_network1 = '2001:db8:2::/48'
+  additional_v4_network2 = '198.51.100.0/24'
+  additional_v6_network2 = '2001:db8:3::/48'
   on(router1, 'sysctl net.ipv6.conf.all.disable_ipv6=0')
   on(router2, 'sysctl net.ipv6.conf.all.disable_ipv6=0')
   on(router1, "ip -6 addr add #{router1_ip6}/64 dev eth0", acceptable_exit_codes: [0, 2])
@@ -40,8 +42,16 @@ describe 'quagga class ACLs ' do
     class { '::quagga::bgpd':
       my_asn => #{router2_asn},
       router_id => '#{router2_ip}',
-      networks4 => [ '#{ipv4_network}', '#{additional_v4_network}'],
-      networks6 => [ '#{ipv6_network}', '#{additional_v6_network}'],
+      networks4 => [ 
+        '#{ipv4_network}',
+        '#{additional_v4_network1}',
+        '#{additional_v4_network2}',
+        ],
+      networks6 => [
+        '#{ipv6_network}',
+        '#{additional_v6_network1}',
+        '#{additional_v6_network2}'
+        ],
       peers => {
         '#{router1_asn}' => {
           'addr4'             => ['#{router1_ip}'],
@@ -91,6 +101,7 @@ describe 'quagga class ACLs ' do
     end
     describe command('vtysh -c \'show ip bgp \'') do
       its(:stdout) { is_expected.not_to match(%r{192.0.2.0}) }
+      its(:stdout) { is_expected.not_to match(%r{198.51.100.0}) }
       its(:stdout) { is_expected.not_to match(%r{0.0.0.0}) }
     end
     describe command('vtysh -c \'show ipv6 bgp sum\'') do
@@ -100,7 +111,8 @@ describe 'quagga class ACLs ' do
       its(:stdout) { is_expected.to match(%r{BGP state = Established}) }
     end
     describe command('vtysh -c \'show ipv6 bgp\'') do
-      its(:stdout) { is_expected.not_to match(%r{#{additional_v6_network}}) }
+      its(:stdout) { is_expected.not_to match(%r{#{additional_v6_network1}}) }
+      its(:stdout) { is_expected.not_to match(%r{#{additional_v6_network2}}) }
       its(:stdout) { is_expected.not_to match(%r{::\/0}) }
     end
   end
@@ -129,11 +141,74 @@ describe 'quagga class ACLs ' do
       sleep(10)
     end
     describe command('vtysh -c \'show ip bgp \'') do
-      its(:stdout) { is_expected.to match(%r{192.0.2.0\s+#{router2_ip}\s+\d+\s+\d+\s+#{router2_asn}}) }
+      its(:stdout) do
+        is_expected.to match(
+          %r{192.0.2.0\s+#{router2_ip}\s+\d+\s+\d+\s+#{router2_asn}}
+        ) 
+      end
+      its(:stdout) do
+        is_expected.to match(
+          %r{198.51.100.0\s+#{router2_ip}\s+\d+\s+\d+\s+#{router2_asn}}
+        ) 
+      end
       its(:stdout) { is_expected.not_to match(%r{0.0.0.0}) }
     end
     describe command('vtysh -c \'show ipv6 bgp \'') do
-      its(:stdout) { is_expected.to match(%r{#{additional_v6_network}\s+#{router2_ip6}\s+\d+\s+\d+\s+#{router2_asn}}) }
+      its(:stdout) do
+        is_expected.to match(
+          %r{#{additional_v6_network1}\s+#{router2_ip6}\s+\d+\s+\d+\s+#{router2_asn}}
+        )
+      end
+      its(:stdout) do
+        is_expected.to match(
+          %r{#{additional_v6_network2}\s+#{router2_ip6}\s+\d+\s+\d+\s+#{router2_asn}}
+        )
+      end
+      its(:stdout) { is_expected.not_to match(%r{::\/0}) }
+    end
+  end
+  context 'all with rejected networks' do
+    pp1 = <<-EOF
+    class { '::quagga': }
+    class { '::quagga::bgpd':
+      my_asn => #{router1_asn},
+      router_id => '#{router1_ip}',
+      rejected_v4 => ['#{additional_v4_network1}'],
+      rejected_v6 => ['#{additional_v6_network1}'],
+      peers => {
+        '#{router2_asn}' => {
+          'addr4'          => ['#{router2_ip}'],
+          'addr6'          => ['#{router2_ip6}'],
+          'desc'           => 'TEST Network',
+          'inbound_routes' => 'all',
+          }
+      }
+    }
+    EOF
+    it 'appling all acl' do
+      apply_manifest(pp1, catch_failures: true)
+    end
+    it 'clean acl run' do
+      expect(apply_manifest(pp1, catch_failures: true).exit_code).to eq 0
+      # allow peers to configure and establish
+      sleep(10)
+    end
+    describe command('vtysh -c \'show ip bgp \'') do
+      its(:stdout) { is_expected.not_to match(%r{192.0.2.0}) }
+      its(:stdout) do
+        is_expected.to match(
+          %r{198.51.100.0\s+#{router2_ip}\s+\d+\s+\d+\s+#{router2_asn}}
+        ) 
+      end
+      its(:stdout) { is_expected.not_to match(%r{0.0.0.0}) }
+    end
+    describe command('vtysh -c \'show ipv6 bgp \'') do
+      its(:stdout) { is_expected.not_to match(%r{#{additional_v6_network1}}) }
+      its(:stdout) do
+        is_expected.to match(
+          %r{#{additional_v6_network2}\s+#{router2_ip6}\s+\d+\s+\d+\s+#{router2_asn}}
+        )
+      end
       its(:stdout) { is_expected.not_to match(%r{::\/0}) }
     end
   end
@@ -166,7 +241,7 @@ describe 'quagga class ACLs ' do
       its(:stdout) { is_expected.to match(%r{0.0.0.0\s+#{router2_ip}\s+\d+\s+#{router2_asn}}) }
     end
     describe command("vtysh -c \'show ipv6 bgp \'") do
-      its(:stdout) { is_expected.not_to match(%r{#{additional_v6_network}}) }
+      its(:stdout) { is_expected.not_to match(%r{#{additional_v6_network1}}) }
       its(:stdout) { is_expected.to match(%r{::\/0\s+#{router2_ip6}\s+\d+\s+#{router2_asn}}) }
     end
   end
